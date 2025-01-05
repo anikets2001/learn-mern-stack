f13a126aa76f9a2f1caefa3bf7c05ba0aadde34a
Aniket Singh <aniketsingh90126@gmail.com>
2025-01-05 22:43:52 +0530

    learn blocking code

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res)=> {
-    console.log('request event');
-    res.end('Hello world')
-})
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("home page.");
+  } else if (req.url === "/about") {
+    // for (let i = 0; i < 1000; i++) {
+    //   for (let j = 0; j < 1000; j++) {
+    //     console.log(i, j);
+    //   }
+    // }
+    res.end("about page");
+  } else if (req.url === "/contact") {
+    res.end("contact page");
+  } else res.end(`Error page: Go back to Homepage...`);
+});
 
-server.listen(5000, ()=> {
-    console.log('Server listening on port:5000....')
-})
+server.listen(5000, () => {
+  console.log("listening server on port 5000..");
+});
