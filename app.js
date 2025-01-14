bfefb3928e8258a009db1e37772fb2e7e2df4cfb
Aniket Singh <aniketsingh90126@gmail.com>
2025-01-14 22:32:34 +0530

    feat: learn htttp status code and writeHead

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
+  const { url } = req;      //destructure url from req object
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
     res.end("homepage");
-  } else if (req.url === "/about") {
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
     res.end("about page");
   } else {
-    res.end(`<h1>Error page</h1>`);
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end(`<h1>Page not found!</h1>`);
   }
 });
 
-server.listen(5000, ()=> {
-    console.log('starting server....')
-})
+server.listen(5000, () => {
+  console.log("starting server....");
+});
